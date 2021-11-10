37f3cc93e6b0cf4f2561de5bfc8c33dbb15ecf0c
marinambur <marinaburykina@gmail.com>
2021-11-10 11:00:35 +0200

    readme added

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,10 @@
   <div class="about pa-7">
     <h1>About this awesome ToDoList</h1>
     <p>Hi! I'm the best ToDoList you've ever seen!!!</p>
-    <p>Lorem ipsum dolor sit amet, mei adipisci recusabo scriptorem et. Vix moderatius liberavisse ut, eu mazim prodesset usu. Ex sumo possit audire usu, vis et ubique salutatus. Scaevola dissentiet mel et. Ius putent propriae concludaturque cu, utamur oblique ceteros in mea, porro ipsum suscipiantur ei usu.
-    </p>
-    <p>Lorem ipsum dolor sit amet, mei adipisci recusabo scriptorem et. Vix moderatius liberavisse ut, eu mazim prodesset usu. Ex sumo possit audire usu, vis et ubique salutatus. Scaevola dissentiet mel et. Ius putent propriae concludaturque cu, utamur oblique ceteros in mea, porro ipsum suscipiantur ei usu.
-    </p>
-    <p>Lorem ipsum dolor sit amet, mei adipisci recusabo scriptorem et. Vix moderatius liberavisse ut, eu mazim prodesset usu. Ex sumo possit audire usu, vis et ubique salutatus. Scaevola dissentiet mel et. Ius putent propriae concludaturque cu, utamur oblique ceteros in mea, porro ipsum suscipiantur ei usu.
-    </p>
+    <p>1)Add task</p>
+    <p>2)Reload page - task is still here? Of course!</p>
+    <p>3)Click on task when it's done! Magic!</p>
+    <p>4)Delete your task if you want to do nothing today! Go to bed!</p>
+    <p>Forgot to say: picture is new every time you see the app! Awesome!</p>
   </div>
 </template>
